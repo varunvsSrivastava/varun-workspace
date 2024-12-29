452a2b5f2e35ac2237710b960d369e71756c9bbf
varunvsSrivastava <varunvs0000@gmail.com>
2024-12-29 13:30:57 +0530

    add

---
+++ java-work/add.java
@@ -0,0 +1,13 @@
+
+import java.util.*;
+public class add{
+	public static void main(String[] args){
+		int num1;
+		int num2;
+		Scanner sc=new Scanner(System.in);
+		num1=sc.nextInt();
+		num2=sc.nextInt();
+		System.out.println("Result: "+(num1+num2));
+		sc.close();
+	}
+}
