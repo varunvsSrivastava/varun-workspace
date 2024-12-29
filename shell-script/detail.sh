f099c8f379e09c860433450460795302a953e8c7
varunvsSrivastava <varunvs0000@gmail.com>
2024-12-29 14:00:04 +0530

    script file

---
+++ shell-script/detail.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+name="Varun Srivastav"
+course="MCA"
+batch="Batch 6"
+section="Section B"
+echo "My detial"
+echo "My name is $name pursuing $course . My section is $section and batch $batch"
